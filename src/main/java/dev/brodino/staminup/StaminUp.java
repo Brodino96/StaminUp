7be1da92bc930a7e478934cfa9ae09d55e4e5b32
Brodino <brodino96@gmail.com>
2026-05-25 03:44:33 +0200

    chore(cleanup): ran import cleanup

--- src/main/java/dev/brodino/staminup/StaminUp.java
+++ src/main/java/dev/brodino/staminup/StaminUp.java
@@ -1,18 +1,10 @@
 package dev.brodino.staminup;
 
 import dev.brodino.staminup.effects.EffectHandler;
-import dev.brodino.staminup.network.StaminUpPackets;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 public class StaminUp implements ModInitializer {
 
