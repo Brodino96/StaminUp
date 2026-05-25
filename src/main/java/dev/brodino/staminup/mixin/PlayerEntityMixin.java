f3dd13bef7de098c6fe86db235bd6a00168b6ec7
Brodino <brodino96@gmail.com>
2026-05-25 03:35:43 +0200

    feat(jump): optimized jump mixin

--- src/main/java/dev/brodino/staminup/mixin/PlayerEntityMixin.java
+++ src/main/java/dev/brodino/staminup/mixin/PlayerEntityMixin.java
@@ -1,31 +1,24 @@
 package dev.brodino.staminup.mixin;
 
-import dev.brodino.staminup.StaminUp;
 import dev.brodino.staminup.StaminaHandler;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.UUID;
-
-@Mixin(PlayerEntity.class)
+@Mixin(LivingEntity.class)
 public class PlayerEntityMixin {
 
-    @Environment(EnvType.SERVER)
     @Inject(method = "jump", at = @At("HEAD"), cancellable = true)
     public void jump(CallbackInfo ci) {
-        PlayerEntity p = (PlayerEntity) (Object) this;
-        ServerPlayerEntity player = (ServerPlayerEntity) (Object) p;
-
-        boolean jumped = StaminaHandler.tryJump(player);
-        if (!jumped) {
-            ci.cancel();
+        if (!((Object) this instanceof ServerPlayerEntity player)) {
             return;
+        }
+
+        if (!StaminaHandler.tryJump(player)) {
+            ci.cancel();
         }
     }
 }
