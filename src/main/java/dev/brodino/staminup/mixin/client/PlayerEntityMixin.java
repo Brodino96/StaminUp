c05b82de79b9b53e0bc526f5d273253cb8a398c8
Brodino <brodino96@gmail.com>
2026-05-25 03:35:08 +0200

    feat(client): optimized jump mixin

--- src/main/java/dev/brodino/staminup/mixin/client/PlayerEntityMixin.java
+++ src/main/java/dev/brodino/staminup/mixin/client/PlayerEntityMixin.java
@@ -3,23 +3,26 @@ package dev.brodino.staminup.mixin.client;
 import dev.brodino.staminup.client.StaminUpClient;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerEntity.class)
+@Environment(EnvType.CLIENT)
+@Mixin(LivingEntity.class)
 public class PlayerEntityMixin {
 
-    @Environment(EnvType.CLIENT)
     @Inject(method = "jump", at = @At("HEAD"), cancellable = true)
     public void jump(CallbackInfo ci) {
+        if (!((Object) this instanceof ClientPlayerEntity)) return;
+
         if (!StaminUpClient.canJump()) {
             ci.cancel();
             return;
         }
 
-        StaminUpClient.stamina -= StaminUpClient.jumpConsumption;
+        StaminUpClient.stamina = Math.max(0, StaminUpClient.stamina - StaminUpClient.jumpConsumption);
     }
 }
